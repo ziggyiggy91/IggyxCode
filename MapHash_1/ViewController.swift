f4be32e091cd3254f5115f88ae5ca9b24193bb06
Ismael Garcia <Ziggy@ismaels-mac.local>
2016-12-23 23:08:39 -0800

    Initial Commit

---
+++ MapHash_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapHash_1
+//
+//  Created by Ismael Garcia on 12/23/16.
+//  Copyright © 2016 Ismael Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
